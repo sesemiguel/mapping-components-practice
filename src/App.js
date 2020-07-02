b7e36c0d80d07d6344c16bfad7d09d5ad6eec74b
Sese <sese.miguel9@gmail.com>
2020-07-02 20:54:18 +0800

    Converted into class based components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from "react"
 import Product from "./components/Product.js"
 import products from './data/products.js'
 
-function App(){
+class App extends React.Component {
+    render() {
+        const productComponents = products.map(productData => <Product key={productData.id} product={productData} />)
 
-    const productComponents = products.map(productData => <Product key={productData.id} product={productData} />)
-
-    return(
-        <>
-        {productComponents}
-        </>
-    )
+        return (
+            <>
+                {productComponents}
+            </>
+        )
+    }
 }
 
 export default App
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,17 @@
 import React from "react"
 
-function Product(props) {
-    return (
-        <div className="product-card">
-            <h2>{props.product.name}</h2>
-            <h4>Price: ${props.product.price}</h4>
-            <p>{props.product.description}</p>
-            <hr />
-        </div>
-    )
+class Product extends React.Component {
+    render() {
+
+        return (
+            <>
+                <h2>{this.props.product.name}</h2>
+                <h4>Price: ${this.props.product.price}</h4>
+                <p>{this.props.product.description}</p>
+                <hr />
+            </>
+        )
+    }
 }
 
 export default Product
